b4e7aa099697024d6f91a5897e86949427415760
Drowrin <drowrin@gmail.com>
2021-01-04 14:54:14 -0500

    clean up EntityBrief

---
+++ src/components/EntityBrief.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div class="card hoverable grey darken-3">
+            <div class="card-content text-emphasis">
+                <span class="card-title" style="margin-bottom: 0px;">
+                    {{d.name}}
+                        <span
+                            class="text-disabled"
+                            style="margin: 5px; font-size: 15px;"
+                            v-for="cat in d.categories"
+                            :key="cat">
+                            {{cat.name}}
+                        </span>
+                </span>
+                
+                <table><tbody>
+                    <tr>
+                        <td class="left" v-if="d.equipment && d.equipment.weight">{{d.equipment.weight}}</td>
+                        <td class="left" v-if="d.equipment && d.equipment.cost">{{d.equipment.cost}}</td>
+                        <td class="right" v-if="d.weapon">{{d.weapon.damage}} {{d.weapon.type}}</td>
+                        <td class="right" v-if="d.armor">{{d.armor.ac}} AC</td>
+                    </tr>
+                    <tr v-if="d.equipment && d.equipment.properties.length > 0">
+                        <td class="left" v-for="prop in d.equipment.properties" :key="prop">
+                            {{prop.display}}
+                        </td>
+                    </tr>
+                    <tr v-if="d.vehicle"><td>
+                        <div class="left" style="margin-right: 5px;" v-if="d.vehicle.speed">
+                            <span class="text-disabled">|</span>
+                            Speed: {{d.vehicle.speed}}
+                        </div>
+                        <div class="left" style="margin-right: 5px;" v-if="d.vehicle.capacity">
+                            <span class="text-disabled">|</span>
+                            Capacity: {{d.vehicle.capacity}}
+                        </div>
+                        <div class="left" style="margin-right: 5px;" v-if="d.vehicle.workers">
+                            <span class="text-disabled">|</span>
+                            Workers: {{d.vehicle.workers}}
+                        </div>
+                    </td></tr>
+                </tbody></table>
+
+                <p class="card-p" v-if="d.tool">{{d.tool.proficiency}}</p>
+
+                <p class="card-p" v-if="d.description">{{d.description}}</p>
+
+                <div v-if="d.property">
+                    <h6 v-if="d.property.display" class="text-emphasis">{{d.property.display}}</h6>
+                    <p>{{d.property.description}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        d: {
+            required: true,
+        }
+    }
+}
+</script>
