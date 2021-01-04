32f88c173f90aebb9c1dac9c2557fb1010e92433
Drowrin <drowrin@gmail.com>
2021-01-04 00:28:27 -0500

    use masony for search cards

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,15 @@
 <template>
-    <div v-if="results" class="row">
-        <EntityBrief class="col s12 l6 xl4" v-for="result in results" :key="result" :d="result" />
+    <div v-if="results" class="row grid">
+        <EntityBrief class="col s12 l6 xl4 grid-item" v-for="result in results" :key="result" :d="result" />
     </div>
     <div v-else>no results</div>
 </template>
 
 <script>
 import { inject, ref, watch } from 'vue';
+import Masonry from 'masonry-layout';
+
+var msnry;
 
 export default {
     setup() {
@@ -19,7 +22,6 @@ export default {
         function getResults(q) {
             if (q) {
                 results.value = fuse.value.search(q).map(r => searchArray.value[r.refIndex]);
-                console.log(results.value);
             }
             else results.value = [];
         }
@@ -29,6 +31,14 @@ export default {
         watch(() => searchTerm.value, getResults);
 
         return { results };
+    },
+    mounted() {
+        msnry = new Masonry('.grid', {});
+    },
+    updated() {
+        console.log('updated');
+        msnry.reloadItems();
+        msnry.layout();
     }
 }
 </script>
