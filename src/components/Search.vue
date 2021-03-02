9053c6a999f5b1fd1d0c358aa8ef7f03b9d207e7
Drowrin <drowrin@gmail.com>
2021-03-02 13:07:23 -0500

    refactor grid into its own component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,27 +1,11 @@
 <template>
-    <div
-        ref="grid"
-        v-if="results"
-        style="margin-bottom: 0px !important; padding-top: 10px !important"
-    >
-        <div class="grid-sizer" :style="gridSize" />
-        <EntityBrief
-            :style="gridSize + `padding-right: 10px; padding-left: 10px;`"
-            v-for="result in results"
-            :key="result"
-            :d="result.item"
-        />
-    </div>
-    <div v-else>no results</div>
+    <Grid>
+        <EntityBrief v-for="result in results" :key="result" :d="result.item" />
+    </Grid>
 </template>
 
 <script>
-import { inject, nextTick, onMounted, onUpdated, ref, watch } from 'vue';
-import Masonry from 'masonry-layout';
-import imagesLoaded from 'imagesloaded';
-
-var msnry;
-var reloadMsnry = true;
+import { inject, ref, watch } from 'vue';
 
 export default {
     setup() {
@@ -37,64 +21,13 @@ export default {
         function getResults(q) {
             if (q) searchWorker.postMessage(q);
             else results.value = [];
-
-            reloadMsnry = true;
         }
 
         getResults(searchTerm.value);
 
         watch(() => searchTerm.value, getResults);
 
-        const gridSize = ref('');
-        const grid = ref(null);
-
-        function updateMsnry() {
-            msnry.reloadItems();
-            msnry.layout();
-        }
-
-        var lastCount = 0;
-
-        function updateWidth() {
-            var w = grid.value.clientWidth;
-
-            var count = Math.max(1, Math.floor(w / 450));
-
-            if (lastCount != count) {
-                gridSize.value = `width: ${100 / count}%;`;
-                lastCount = count;
-                requestAnimationFrame(() => {
-                    requestAnimationFrame(() => {
-                        updateMsnry();
-                        imagesLoaded(grid.value, updateMsnry);
-                    });
-                });
-            }
-        }
-
-        var loaded = false;
-
-        onMounted(function () {
-            msnry = new Masonry(grid.value, {
-                columnWidth: '.grid-sizer',
-                percentPosition: true,
-                transitionDuration: 0,
-            });
-
-            window.onresize = updateWidth;
-
-            setTimeout(updateWidth, 25);
-        });
-
-        onUpdated(function () {
-            if (reloadMsnry) {
-                updateMsnry();
-                imagesLoaded(grid.value, updateMsnry);
-                reloadMsnry = false;
-            }
-        });
-
-        return { results, gridSize, grid };
+        return { results };
     },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import App from './App.vue';
 
 import EntityBreif from './components/EntityBrief.vue';
 import EntityLink from './components/EntityLink.vue';
+import Grid from './components/Grid.vue';
 
 const app = createApp(App).use(router);
 
 app.component('EntityBrief', EntityBreif);
 app.component('EntityLink', EntityLink);
+app.component('Grid', Grid);
 
 app.mount('#app');
