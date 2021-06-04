9537e739e8b794743d1d511737350fded871494c
drowrin <drowrin@gmail.com>
2021-06-04 01:46:35 -0400

    clean up article display

--- src/components/entities/Article.vue
+++ src/components/entities/Article.vue
@@ -1,45 +1,48 @@
 <template>
-    <EntityCard :entity="entity" :description="card" />
+    <EntityCard :entity="entity" v-if="card" />
 
-    <div id="article-section" v-if="!card">
-        <Card v-for="[n, d] in Object.entries(entity.article)" :key="n" class="article-card">
-            <template #title>
-                {{ n }}
-            </template>
-            <template #content>
-                <Markdown :source="d.rendered" />
-            </template>
-        </Card>
+    <div v-else>
+        <EntityCard :entity="entity" :description="false" :style="style" />
+
+        <div id="article-wrapper" ref="wrapper">
+            <Card v-for="[n, d] in Object.entries(entity.article)" :key="n" class="article-card">
+                <template #title>
+                    {{ n }}
+                </template>
+                <template #content>
+                    <Markdown :source="d.rendered" />
+                </template>
+            </Card>
+        </div>
     </div>
 </template>
 
 <style>
-#article-section {
-    padding-top: 5px;
+#article-wrapper {
     margin: auto;
+    display: flex;
+    flex-wrap: wrap;
     justify-content: center;
     align-items: flex-start;
 }
 
 .article-card {
-    margin: 5px auto;
+    margin: 10px 0 0;
     height: fit-content;
+    width: 100%;
 }
 
-@media (min-width: 990px) {
-    #article-section {
-        display: flex;
-        flex-wrap: wrap;
-    }
-
+@media (min-width: 1011px) {
     .article-card {
+        margin: 10px 5px 0;
         width: 720px;
-        margin: 5px;
     }
 }
 </style>
 
 <script>
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
 import Card from 'primevue/card';
 import EntityCard from './parts/EntityCard.vue';
 import Markdown from '../Markdown.vue';
@@ -57,6 +60,43 @@ export default {
             type: Boolean,
             default: false,
         },
+    },
+    setup(props) {
+        let wrapper = ref(null);
+        let style = ref('');
+
+        function updateWidth() {
+            if (wrapper.value !== null) {
+                let w = wrapper.value.clientWidth;
+
+                if (w <= 730) {
+                    style.value = '';
+                } else {
+                    let cols = Math.floor(w / 730);
+                    let count = wrapper.value.querySelectorAll('.article-card').length;
+                    let mult = Math.min(count, cols);
+
+                    style.value = `margin: auto; width: ${730 * mult - 10}px`;
+                }
+            }
+        }
+
+        const ro = new ResizeObserver(updateWidth);
+
+        onMounted(() => {
+            if (!props.card) {
+                ro.observe(wrapper.value);
+                updateWidth();
+            }
+        });
+
+        onBeforeUnmount(() => {
+            if (!props.card) {
+                ro.unobserve(wrapper.value);
+            }
+        });
+
+        return { wrapper, style };
     },
 };
 </script>
